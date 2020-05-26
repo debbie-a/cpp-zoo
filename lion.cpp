d0d44cfe1971948310db8759d9d120009dce5b43
d-a-mby <207425349@mby.co.il>
2020-05-26 20:02:45 +0300

    first commit

--- lion.cpp
+++ lion.cpp
@@ -1,6 +1,6 @@
 #include "lion.h"
 
-const std::string Lion::s_species = " Panthera leo";
+const std::string Lion::s_species = "Panthera leo";
 const unsigned short int Lion::s_lifeExpectancy = 50;
 const std::vector<std::string> Lion::s_continents{continents[0],continents[2]};
 const std::vector<std::string> Lion::s_foodEats{animalFood[3],animalFood[4]};
